7bd5b4a3ed63f1c6cdafb98b90969ad9fd73d01d
Pedro Durães <pedrofilipeduraes@hotmail.com>
2017-03-24 14:33:22 +0000

    try fetching current game info from riot games API

--- Server/controllers/player.js
+++ Server/controllers/player.js
@@ -1,19 +1,28 @@
-var getPlayer = function (request, reply){
-  reply('this is the info route for ' + request.params.player);
-};
-
-var huniInfo = {
-  "sktt1huni": {
-    "summonerLevel": 30,
-    "profileIconId": 1445,
-    "revisionDate": 1490266637000,
-    "id": 39010126,
-    "name": "SKT T1 Huni"
-  }
-}
+const superagent = require('superagent');
+const config = require('../config/config');
 
 var accounts = [];
-accounts.push(39010126);
+accounts.push({
+  name: 'Velze',
+  id: 35162075,
+  status: {}
+});
+
+var resps = 0;
+var getPlayer = function (request, reply){
+  resps = 0;
+  accounts.forEach(function(account){
+    var url = 'https://euw.api.riotgames.com/observer-mode/rest/consumer/getSpectatorGameInfo/EUW1/' + account.id + '?api_key=' + config.riotApiKey;
+    superagent.get(url, function(err, res){
+      resps++;
+      if (err) account.status = 'offline';
+      else account.status = res;
+      console.log(res.text);
+      if(resps == accounts.length)
+        reply(accounts);
+    });
+  })
+};
 
 
 module.exports.handler = getPlayer;
