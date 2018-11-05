18e8edd427ce8d1e68a6b953e681cddf547b62af
Runtime <runtime@Runtimes-MacBook-Pro-5.local>
2018-11-05 10:45:14 +0000

    set up get routes for users and players. Hook players with riot API

--- Server/controllers/user.js
+++
@@ -1,5 +0,0 @@
-var getUser = function (request, reply){
-  reply('this is the profile page for user ' + request.params.user);
-};
-
-module.exports.handler = getUser;
--- Server/controllers/users.js
+++
@@ -1,5 +0,0 @@
-var getUsers = function (request, reply){
-  reply('this is where the users collection will be displayed');
-};
-
-module.exports.handler = getUsers;
